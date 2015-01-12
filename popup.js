c5b728c1afd9221769f4fdeca14b1942321f1f18
cmacca <definitestudios@gmail.com>
2015-01-11 18:58:06 -0700

    initialized first section of Js w/o perfect CSS

--- popup.js
+++ popup.js
@@ -1,20 +1,29 @@
-var notedOverallHours = document.getElementById("overallHours").value * 60;
-var courseNumberTotal = document.getElementById("courseAmount").value;
-var counter = 0;
+window.onload = function () {
 
-function createInput() {
-	var newForm = document.getElementById("hourForm");
-	document.getElementById("initialForm").style.display = "none";
-	document.getElementById("hourForm").style.display = "block";
+	document.getElementById("initialForm").addEventListener('submit', function (e) {
+    	e.preventDefault();
+    	createInput();
+	});
 
+	function createInput() {
+  	var notedOverallHours = document.getElementById("overallHours").value * 60; 
+  	var courseNumberTotal = document.getElementById("courseAmount").value;
+  	var newForm = document.getElementById("hourForm");
+  
+  	document.getElementById("initialForm").style.display = "none";
+		document.getElementById("hourForm").style.display = "block";
+    
+  	// Add our elements
+  	for (i = 1; i <= courseNumberTotal; i++) {
+			var child = document.createElement('span');
+			child.innerHTML = "<label>Course #" + (i) + "</label>" + "<input type='number' name='courseHours-"+ i+"' class='newInputs' min='1' max='9' step='1' value='1'/>";
+			newForm.appendChild(child);
+ 	 }
 
-	for (i = 0; i <= courseNumberTotal; i++) {
-		newForm.innerHTML = "<label>Course #" + (counter + 1) + "</label>" + "<input type='number' name='courseHours' class='newInputs' min='1' max='9' step='1' value='1'/>";
-		counter++;
+  	// Add our button
+  	var button = document.createElement('span');
+  	button.innerHTML = "<input type='submit' value='submit'/>";
+  	newForm.appendChild(button);
 	}
-	newForm.innerHTML = "<input type='submit' value='submit'/>";
 
-}
-
-
-
+};
